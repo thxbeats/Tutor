f563ff4ecd8392637047d4de961b891503923d50
thxbeats <timav2914@gmail.com>
2024-10-10 00:53:56 +0300

    so many job...

--- src/main/java/org/education/TutoringPlatformApplication.java
+++ src/main/java/org/education/TutoringPlatformApplication.java
@@ -2,8 +2,7 @@ package org.education;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+
 
 @SpringBootApplication
 public class TutoringPlatformApplication {
