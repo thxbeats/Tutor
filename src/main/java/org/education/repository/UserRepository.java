16327da0a3355e7b629f1f765ae7f095aa16c73c
thxbeats <timav2914@gmail.com>
2024-11-10 14:56:07 +0300

    Translated succ registration card

--- src/main/java/org/education/repository/UserRepository.java
+++ src/main/java/org/education/repository/UserRepository.java
@@ -2,12 +2,27 @@ package org.education.repository;
 
 import org.education.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
     Optional<User> findByEmail(String email);
+
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM auth.user_roles WHERE user_id IN :ids", nativeQuery = true)
+    void deleteUserRolesByUserIds(@Param("ids") List<Long> ids);
+
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM auth.users WHERE id IN :ids", nativeQuery = true)
+    void deleteUsersByIds(@Param("ids") List<Long> ids);
 }
