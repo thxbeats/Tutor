cfb5a23fbc56abd41ce022c60c177bc133dcae52
thxog <timav2914@gmail.com>
2025-01-27 22:05:10 +0300

    Add subject Cards to Main page

--- src/main/java/org/education/controllers/HomeController.java
+++ src/main/java/org/education/controllers/HomeController.java
@@ -1,19 +1,28 @@
 package org.education.controllers;
 
 
+import org.education.model.Subject;
+import org.education.repository.SubjectRepository;
+import org.education.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.security.Principal;
+import java.util.List;
 import java.util.Locale;
 
 @Controller
 public class HomeController {
+
+    @Autowired
+    private SubjectRepository subjectRepository;
 
     @GetMapping("/")
     public String hello(Model model, Locale locale, Principal principal) {
@@ -30,11 +39,14 @@ public class HomeController {
         } else {
             System.out.println("No authentication information available.");
         }
+        List<Subject> SubjectList = subjectRepository.findAll();
+        model.addAttribute("subjectElement", SubjectList);
         return "hello";
     }
 
     @GetMapping("/welcome")
-    public String welcomePage(@RequestParam(name = "msg", required = false, defaultValue = "Welcome to the Turoring Platform") String msg,Principal principal, Model model) {
+    public String welcomePage(@RequestParam(name = "msg", required = false,
+            defaultValue = "Welcome to the Tutoring Platform") String msg,Principal principal, Model model) {
         model.addAttribute("message", msg);
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         //User user = (User) authentication.getPrincipal();
